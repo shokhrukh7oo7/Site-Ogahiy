acecdf4404962c75846219f8725836cb0ecae348
shokhrukh7oo7 <shokh7oo70809@gmail.com>
2024-09-11 17:43:27 +0500

    navbar language mobile version fixed

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -13,6 +13,28 @@ languages.forEach((link) => {
 
 // ================================================================================
 
+// Navbar language dropdown mobile version start
+const dropdownItems = document.querySelectorAll(".dropdown-item");
+const dropdownButton = document.querySelector(".dropdown-toggle");
+
+dropdownItems.forEach((item) => {
+  item.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    // Убираем класс "active" у всех элементов
+    dropdownItems.forEach((i) => i.classList.remove("active"));
+
+    // Добавляем класс "active" на выбранный элемент
+    this.classList.add("active");
+
+    // Меняем текст кнопки на текст выбранного элемента
+    dropdownButton.textContent = this.textContent;
+  });
+});
+// Navbar language dropdown mobile version end
+
+// ================================================================================
+
 // Navbar active links start
 const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
 navLinks.forEach((link) => {
